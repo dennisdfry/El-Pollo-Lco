f2812f43eb9383c11b28599450b4cce2d6ae01c9
Dennis Freyer <dennis.dfry@gmail.com>
2024-10-24 17:26:00 +0200

    games.js update

--- classes/world.class.js
+++ classes/world.class.js
@@ -21,10 +21,11 @@ class World {
     ]
     canvas;
     ctx;
-
-    constructor(canvas){
+    keyboard;
+    constructor(canvas, keyboard){
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
+        this.keyboard = keyboard;
         this.draw();
     }
 
--- js/game.js
+++ js/game.js
@@ -1,7 +1,27 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init(){
     canvas = document.getElementById('canvas');
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
 }
+
+
+window.addEventListener("keypress", (e) => {
+    if(e.keycode == 39){
+        keyboard.RIGHT = false;
+    }
+    if(e.keycode == 37){
+        keyboard.LEFT = false;
+    }
+    if(e.keycode == 38){
+        keyboard.up = false;
+    }
+    if(e.keycode == 40){
+        keyboard.DOWN = false;
+    }
+    if(e.keycode == 32){
+        keyboard.SPACE = false;
+    }
+});

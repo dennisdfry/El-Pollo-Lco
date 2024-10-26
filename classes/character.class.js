f490ed5f569c6ac903f851004f0eeee04653e15f
Dennis Freyer <dennis.dfry@gmail.com>
2024-10-26 10:42:45 +0200

    sound include

--- classes/character.class.js
+++ classes/character.class.js
@@ -12,7 +12,7 @@ class Character extends MovableObject {
     ];
     world;
     speed = 10;
-    walking_sound
+    walking_sound = new Audio('audio/walking_sound.mp3')
     constructor() {
         super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
         this.loadImages(this.Walking_Images);
@@ -21,13 +21,16 @@ class Character extends MovableObject {
 
     animate() {
         setInterval(() => {
+            this.walking_sound.pause();
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.play();
             };
             if (this.world.keyboard.LEFT &&  this.x > 0 ) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.play();
             }
             this.world.camera_x = -this.x +100;
         }, 1000 / 60);

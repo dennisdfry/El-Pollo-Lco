c1c35f0d4218a8ce23979d7a72ad2f5a7251abaa
Dennis Freyer <dennis.dfry@gmail.com>
2024-10-31 13:40:28 +0100

    drawableobjectclass

--- classes/moveable-objects.class.js
+++ classes/moveable-objects.class.js
@@ -10,6 +10,7 @@ class MovableObject {
     speedY = 0;
     acceleration = 2.5;
     energy = 100;
+    lastHit = 0;
     applyGravaty() {
         setInterval(() => {
             if (this.isAboveGround() || this.speedY > 0) {
@@ -22,8 +23,14 @@ class MovableObject {
         this.energy -= 5;
         if(this.energy < 0){
             this.energy = 0;
+        }else{
+            this.lastHit = new Date().getTime();
         }
     }
+    isHurt(){
+        let timePassed = new  Date().getTime() - this.lastHit;
+        timePassed = timePassed / 1000;
+        return timePassed < 1;    }
 
     isDead(){
        return this.energy == 0;
